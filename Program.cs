091d30ae54be300cd5f1a0b94ceadb9642d87eec
bosvs <eroinevs@gmail.com>
2024-12-12 01:14:51 +0200

    repaired

--- Program.cs
+++ Program.cs
@@ -1,25 +1,23 @@
-﻿using System;
-using System.Collections;
-
-namespace ConsoleApp3
+﻿namespace Mediator
 {
     // Mainapp test application
-    class MainApp
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             ConcreteMediator m = new ConcreteMediator();
+
             ConcreteColleague1 c1 = new ConcreteColleague1(m);
             ConcreteColleague2 c2 = new ConcreteColleague2(m);
-            ConcreteColleague3 c3 = new ConcreteColleague3(m);  // Додаємо нову колегу
+            ConcreteColleague3 c3 = new ConcreteColleague3(m);
 
             m.Colleague1 = c1;
             m.Colleague2 = c2;
-            m.Colleague3 = c3;  // Додаємо нову колегу в медіатор
+            m.Colleague3 = c3;
 
             m.Send("How are you?", c1);
             m.Send("Fine, thanks", c2);
-            m.Send("Hello!", c3); 
+            m.Send("Hello!", c3);
 
             // Wait for user
             Console.Read();
@@ -37,7 +35,7 @@ namespace ConsoleApp3
     {
         private ConcreteColleague1 colleague1;
         private ConcreteColleague2 colleague2;
-        private ConcreteColleague3 colleague3;  // Додаємо поле для нової колеги
+        private ConcreteColleague3 colleague3;
 
         public ConcreteColleague1 Colleague1
         {
@@ -49,7 +47,7 @@ namespace ConsoleApp3
             set { colleague2 = value; }
         }
 
-        public ConcreteColleague3 Colleague3  // Додаємо властивість для нової колеги
+        public ConcreteColleague3 Colleague3
         {
             set { colleague3 = value; }
         }
@@ -58,15 +56,18 @@ namespace ConsoleApp3
         {
             if (colleague == colleague1)
             {
-                colleague1.Notify(message);
+                colleague2.Notify(message);
+                colleague3.Notify(message);
             }
             else if (colleague == colleague2)
             {
-                colleague2.Notify(message);
-            }
-            else
-            {
+                colleague1.Notify(message);
                 colleague3.Notify(message);
+            }
+            else if (colleague == colleague3)
+            {
+                colleague1.Notify(message);
+                colleague2.Notify(message);
             }
         }
     }
@@ -75,7 +76,7 @@ namespace ConsoleApp3
     abstract class Colleague
     {
         protected Mediator mediator;
-        // Constructor
+
         public Colleague(Mediator mediator)
         {
             this.mediator = mediator;
@@ -85,7 +86,6 @@ namespace ConsoleApp3
     // "ConcreteColleague1"
     class ConcreteColleague1 : Colleague
     {
-        // Constructor
         public ConcreteColleague1(Mediator mediator) : base(mediator) { }
 
         public void Send(string message)
@@ -102,7 +102,6 @@ namespace ConsoleApp3
     // "ConcreteColleague2"
     class ConcreteColleague2 : Colleague
     {
-        // Constructor
         public ConcreteColleague2(Mediator mediator) : base(mediator) { }
 
         public void Send(string message)
@@ -116,10 +115,9 @@ namespace ConsoleApp3
         }
     }
 
-    // "ConcreteColleague3" - новий клас
+    // "ConcreteColleague3"
     class ConcreteColleague3 : Colleague
     {
-        // Constructor
         public ConcreteColleague3(Mediator mediator) : base(mediator) { }
 
         public void Send(string message)
